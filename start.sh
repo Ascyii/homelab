d09b5fed4fe6f533aed1dad94fe6cc51281fd6be
Jonas Hahn <jonashahn1@gmx.net>
2025-08-30 00:01:46 +0000

    Some new selfhostet services and working file server to be filled with all the data

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ SESSION_NAME="services"
 # Start a new tmux session detached
 tmux new-session -d -s $SESSION_NAME
 
-
 echo "Waiting for tmux to be up."
+
 sleep 2
 
 tmux new-window -t $SESSION_NAME:1 -n dockerd "dockerd"
@@ -14,9 +14,21 @@ tmux new-window -t $SESSION_NAME:2 -n webdav "webdav -c ~/serverconf/webdav.yaml
 tmux new-window -t $SESSION_NAME:3 -n caddy "caddy run --config ~/serverconf/Caddyfile"
 
 echo "Waiting for dockerd to be up."
+
 sleep 10
 
 cd ~/serverconf/filestash
 docker compose up -d
 cd ~
 
+cd ~/serverconf/vaultwarden
+docker compose up -d
+cd ~
+
+cd ~/serverconf/memos
+docker compose up -d
+cd ~
+
+cd ~/serverconf/expenseowl
+docker compose up -d
+cd ~
