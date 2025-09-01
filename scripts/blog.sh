cf98c316d24c83a5ab9e2df6f0faa65c7479ff70
Jonas Hahn <jonashahn1@gmx.net>
2025-09-01 18:22:23 +0000

    Refactor

--- scripts/blog.sh
+++ scripts/blog.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+sleep 2
+
 nix-env -iA nixpkgs.hugo
 
-cd ~/site
-
+cd /root/site
 tmux new-session -d -s hugo
 tmux send-keys -t hugo "hugo server -D --bind "0.0.0.0" --appendPort=false -b "https://hahn1.one" " C-m
